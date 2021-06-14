bcae9b1f8e6ced06a5f8e4133f7c5295ba389677
KHZhanibek <38491882+KHZhanibek@users.noreply.github.com>
2021-06-14 07:25:38 +0600

    Delete routes directory

--- routes/checker.js
+++
@@ -1,7 +0,0 @@
-
-module.exports.checkk = function(data){
-	console.log(data.auth);
-    if(!data.auth)
-    	return 1;
-    return 0;
-}
--- routes/profile.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const verify = require('./routes/verifyToken');
-
-router.post('/', verify, (req, res) =>{
-    res.render('./profile');
-});
-
-module.export = router;
